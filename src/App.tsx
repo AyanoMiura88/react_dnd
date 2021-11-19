7ce7267cc6fdf11eb895262ea259fd1abb83ded5
LAPTOP-HHIDJBON\marie <miura.a@marietta.co.jp>
2021-11-19 21:16:18 +0900

    [add] デフォルトページ指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch as Routes } from "react-router-dom";
 import LoginPage from "./Login/LoginPage";
-import DropArea from './DragAndDrop/DropArea';
+import DropArea from "./DragAndDrop/DropArea";
 
 function App() {
   return (
-    <div className="App">
     <BrowserRouter>
-      <div>
-        <Route path="/" exact component={LoginPage} />
-        <Route path="/MainPage" exact  component={DropArea} />
+      <div className="App">
+        <div>test</div>
+        <Routes>
+          <Route path="/" exact component={LoginPage} />
+          <Route path="/MainPage" exact component={DropArea} />
+          <Route component={LoginPage}/>
+        </Routes>
       </div>
     </BrowserRouter>
-  </div>
   );
 }
 
