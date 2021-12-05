9130e23c4077987b8e1535c37acf689d595d6ca7
ayano miura <miu_lavender228@iCloud.com>
2021-12-05 17:14:16 +0900

    [mod] tableの汎用

---
+++ src/Table/Data.ts
@@ -0,0 +1,14 @@
+export const heading = ["id", "名前", "レベル"];
+
+export const data = [
+  { id: 1, name: "Taro", level: Math.random() },
+  { id: 2, name: "Jiro", level: Math.random() },
+  { id: 3, name: "Saburo", level: Math.random() },
+  { id: 4, name: "Shiro", level: Math.random() },
+  { id: 5, name: "Goro", level: Math.random() },
+  { id: 6, name: "Mutsumi", level: Math.random() },
+  { id: 7, name: "Nanako", level: Math.random() },
+  { id: 8, name: "Yaeko", level: Math.random() },
+  { id: 9, name: "Kyuro", level: Math.random() },
+  { id: 10, name: "juro", level: Math.random() },
+];
--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "./Table.css";
+import { data, heading } from "./Data";
 
 const TablePage = () => {
   return (
-    <div>
-      table
+    <div className="tablePage">
+      <table className="table">
+        <thead className="tableHead">
+          <tr>
+            {heading.map((v,i) => (
+              <th key={i} scope="col">{v}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="tableBody">
+          {data.map((v,i) => (
+            <tr key={i}>
+              <td>{v.id}</td>
+              <td>{v.name}</td>
+              <td>{v.level}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
