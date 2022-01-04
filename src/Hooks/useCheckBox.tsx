a67ae42a5bd5ef55e30c1443b2bd349e98682da2
LILAC\marie <miura.a@marietta.co.jp>
2022-01-04 10:56:27 +0900

    [mod] checkBox一部変更

--- src/Hooks/useCheckBox.tsx
+++ src/Hooks/useCheckBox.tsx
@@ -6,12 +6,12 @@ import React, { useState, useCallback } from "react";
 export const useCheckBox = () => {
   const [checkedValues, setCheckedValues] = useState<string[]>([]);
 
-  const handleChecked = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (checkedValues.includes(e.target.id)) {
       setCheckedValues(checkedValues.filter((checkedValue) => checkedValue !== e.target.id));
     } else {
       setCheckedValues([...checkedValues, e.target.id]);
     }
-  },[]);
+  };
   return { checkedValues, setCheckedValues ,handleChecked };
 };
